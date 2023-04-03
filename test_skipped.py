414fcbef87f1f9738332b023254424d624380f5b
davletgareevmarat <davletgareev.mrt@gmail.com>
2023-04-03 10:46:40 +0500

    add simple tests

---
+++ test_skipped.py
@@ -0,0 +1,26 @@
+import pytest as pytest
+
+
+@pytest.mark.skip
+def test_skipped():
+    pass
+
+
+@pytest.mark.skip
+def test_skipped1():
+    pass
+
+
+@pytest.mark.skip
+def test_skipped2():
+    pass
+
+
+@pytest.mark.skip
+def test_skipped3():
+    pass
+
+
+@pytest.mark.skip
+def test_skipped4():
+    pass
